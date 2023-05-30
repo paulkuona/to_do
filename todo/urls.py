5d65a9efa87cccf5161073508d17414693b546dd
Paul Kuona <pkuona@gmail.com>
2023-05-30 03:00:25 +0100

    todo app updated with task management features

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ADD A NEW TASK
     path('addTask/', views.addTask, name='addTask'),
+
+    # MARK TASK AS DONE
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+
+    # MARK TASK AS UNDONE
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+
+    # EDIT TASK
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+
+    #DELETE TASK
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
+
+
+
 ]
